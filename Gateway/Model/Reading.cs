046d20c4b4f39f117ae60f7517c45896e312383c
39matt <matija.todosijevic.i4@gmail.com>
2026-01-14 19:14:09 +0100

    changes

--- Gateway/Model/Reading.cs
+++ Gateway/Model/Reading.cs
@@ -1,9 +1,15 @@
 namespace Gateway.Model;
 
-public class Temperature
+public class Reading
 {
     public int Id { get; set; }
-    public string Date { get; set; }
-    public int Temperature { get; set; }
-    public string InOut { get; set; }
+    public string Timestamp { get; set; }
+    public string DeviceId { get; set; }
+    public string Co { get; set; }
+    public string Humidity { get; set; }
+    public string Light { get; set; }
+    public string LPG { get; set; }
+    public string Motion { get; set; }
+    public string Smoke { get; set; }
+    public string Temperature { get; set; }
 }
--- Gateway/Repository/IReadingRepository.cs
+++ Gateway/Repository/IReadingRepository.cs
@@ -1,6 +1,13 @@
+using Gateway.Model;
+
 namespace Gateway.Repository;
 
 public interface IReadingRepository
 {
-    
+    IEnumerable<Reading> GetReadings();
+    Reading GetReadingById(int id);
+    void AddReading(Reading reading);
+    void UpdateReading(Reading reading);
+    void DeleteReading(int id);
+    void Save();
 }
